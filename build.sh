012a62286b0f5aa30e1a9812b2d34d1840e70331
cyrneko <cyrus42@disroot.org>
2024-08-23 13:22:37 +0200

    feat: wf-recorder, nautilus

--- build.sh
+++ build.sh
@@ -4,24 +4,14 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
-### Install packages
-
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-# as a reminder, Merkuro is the KDE Calendar, you dummy /s
 curl "https://pkgs.tailscale.com/stable/fedora/tailscale.repo" > /etc/yum.repos.d/tailscale.repo
 curl "https://copr.fedorainfracloud.org/coprs/yalter/niri/repo/fedora-40/yalter-niri-fedora-40.repo" > /etc/yum.repos.d/yalter-niri-fedora-40.repo
 curl "https://copr.fedorainfracloud.org/coprs/errornointernet/packages/repo/fedora-40/errornointernet-packages-fedora-40.repo" > /etc/yum.repos.d/errornointernet-packages-fedora-40.repo
-rpm-ostree install bat eza kitty tailscale zsh niri xwayland-satellite dolphin
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
+rpm-ostree install bat eza kitty tailscale zsh niri xwayland-satellite nautilus wf-recorder
 
 systemctl enable podman.socket
