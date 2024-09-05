d8357d1741a9e23cbf31eab4c2e052f424466bcf
cyrneko <cyrus42@disroot.org>
2024-09-05 14:07:22 +0200

    more :trollface:

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 curl "https://pkgs.tailscale.com/stable/fedora/tailscale.repo" > /etc/yum.repos.d/tailscale.repo
-curl "https://copr.fedorainfracloud.org/coprs/yalter/niri/repo/fedora-40/yalter-niri-fedora-40.repo" > /etc/yum.repos.d/yalter-niri-fedora-40.repo
+curl "https://copr.fedorainfracloud.org/coprs/yalter/niri-git/repo/fedora-40/yalter-niri-git-fedora-40.repo" > /etc/yum.repos.d/yalter-niri-fedora-40.repo
 curl "https://copr.fedorainfracloud.org/coprs/errornointernet/packages/repo/fedora-40/errornointernet-packages-fedora-40.repo" > /etc/yum.repos.d/errornointernet-packages-fedora-40.repo
 curl "https://copr.fedorainfracloud.org/coprs/tofik/nwg-shell/repo/fedora-40/tofik-nwg-shell-fedora-40.repo" > /etc/yum.repos.d/tofik-nwg-shell-fedora-40.repo
 curl "https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-40/alebastr-sway-extras-fedora-40.repo" > /etc/yum.repos.d/alebastr-sway-extras-fedora-40.repo
@@ -19,8 +19,7 @@ rpm-ostree override remove mako alacritty firefox firefox-langpacks foot xarchiv
 
 mkdir git && cd git
 git clone --depth 1 https://codeberg.org/cyrneko/dotfiles
-# cp -r ./dotfiles/niri/ /etc/...
-# TODO: ...where does the config go? will figure out later.
+cp -r ./dotfiles/niri/config.kdl /etc/niri/config.kdl
 cp -r ./dotfiles/waybar/config.jsonc ./dotfiles/waybar/style.css /etc/xdg/waybar/
 cp -r ./dotfiles/fuzzel/fuzzel.ini /etc/xdg/fuzzel/fuzzel.ini
 cp -r ./dotfiles/etc-/doas.conf /etc/doas.conf
